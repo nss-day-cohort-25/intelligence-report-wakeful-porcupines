b4dcbac7fc16fbd08a0574979a4daee0048a2a64
Katheryn Ford <AnEvilHerbivoreLP@gmail.com>
2018-04-18 09:22:16 -0500

    Typos Fixed

--- rapsheetDatabase.js
+++ rapsheetDatabase.js
@@ -1,6 +1,6 @@
 const RapSheetDataBase = 
     {
-    "Crimes": ["Arcorn Smuggling", "Nut Laundering", "Murder in the 1st degree"],
+    "Crimes": ["Acorn Smuggling", "Nut Laundering", "Murder in the 1st degree"],
     "Convictions": ["Tax Evasion", "Arson"],
     "Prisons": ["Shawshank State Prison", "Alcatraz"],
     "Witnesses": ["Morroco Mole", "George Jetson", "Fred Flintstone"],
